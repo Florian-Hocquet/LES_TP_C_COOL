9407e405f712d32576a3342605e7d664ab0f3009
h18000101 <florian.hocquet@etu.univ-amu.fr>
2019-09-16 14:49:30 +0200

    td1

--- tp1.php
+++ tp1.php
@@ -5,22 +5,6 @@ $var2=1.3;
 $var3='Variable3';
 echo"$var1+$var2";
 echo$var1+$var2;
-?>
 
-
-
-<?php
-function start_page($title)
-{
-    echo'<!DOCTYPE html><html lang="fr"><head><title>'.PHP_EOL.$title.'</title></head><body>'.PHP_EOL;
-};
-start_page('titre');
-?>
-
-<?php
-echo date('lFd,Y');
-$jour=date('lFd,Y');
-$jour=date('d/m/Y',strtotime('2020-04-01'));
-
-
+echo $var1 + $var3;
 ?>
