857c7430185043dde25ea48800289d049ddd0a33
h18000101 <florian.hocquet@etu.univ-amu.fr>
2019-09-16 16:28:14 +0200

    td1

---
+++ calcul.php
@@ -0,0 +1,31 @@
+<?php
+include 'functions.inc.php';
+
+start_page('titre');
+
+?>
+
+<?php
+$op1 = $_POST['op1'];
+$op2 = $_POST['op2'];
+$op = $_POST['op'];
+?>
+
+<?php
+if('*' == $op)
+{
+}
+elseif('+' == $op)
+{
+}
+else
+{
+    echo '<br/><strong>opérateur ' . $op . ' non géré </strong>';
+}
+?>
+
+<?php
+
+end_page();
+
+?>
