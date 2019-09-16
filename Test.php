e4ff243ddb9d94adc1680fc4bdbd6ae0e0705efc
h18000101 <florian.hocquet@etu.univ-amu.fr>
2019-09-16 15:09:23 +0200

    td1

---
+++ Test.php
@@ -0,0 +1,22 @@
+<?php
+function start_page($title)
+{
+echo ' <!DOCTYPE html> <html
+    lang="fr"><head><title>' . PHP_EOL . $title . '</title></head><body>' . PHP_EOL
+;
+};
+
+?>
+
+<?php
+
+start_page('titre');
+
+?>
+
+<?php
+function end_page()
+{
+    echo '</body></html>'
+};
+?>
--- tp1.php
+++ tp1.php
@@ -8,32 +8,19 @@
 
 <?php
 
-function start_page($title)
-{
-echo ' <!DOCTYPE html> <html
-    lang="fr"><head><title>' . PHP_EOL . $title . '</title></head><body>' . PHP_EOL
-;
-};
 
-?>
-
-<?php
-
-start_page('titre');
-
-?>
 
 
 <?php
 
 echo'<strong>VoicimonpremierprogrammePHP</strong><br/>'."\n";echo'C\'estpasmalnon?<br/>';
-$var1=6;
-$var2=1.3;
-$var3='Variable3';
-echo"$var1+$var2";
-echo$var1+$var2;
+$var1=6; // déclaration
+$var2=1.3; // déclaration
+$var3='Variable3'; // déclaration
+echo"$var1+$var2"; // affichage de $var1 + $var2
+echo$var1+$var2; // addition de 6 et 1,3
 
-echo $var1 + $var3;
+echo $var1 + $var3; // erreur 6 + Variable3 0
 
 ?>
 
