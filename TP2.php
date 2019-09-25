d2fe931ee424a9547a2e72794a5720a56d9a3357
h18000101 <florian.hocquet@etu.univ-amu.fr>
2019-09-25 09:34:58 +0200

    td1

--- TP2.php
+++ TP2.php
@@ -13,3 +13,22 @@ echo getenv('SERVER_SOFTWARE');
 <?php
 phpinfo();
 ?>
+
+<form action="calcul.php" method="post">
+    <input type="text" name="Id" values="Identifiant"/><br/>
+    <input type="radio " name="Civilité" values="Sexe"/><br/>
+    <input type="text " name="E-mail" values="E-mail"/><br/>
+    <input type="password" name="mdp" values="Mot de passe"/><br/>
+    <input type="password " name="mdp" values="Mot de passe"/><br/>
+    <input type="text " name="tel" values="Téléphone"/><br/>
+    <select name="Nom Pays">
+        <option values="France"> France </option>
+        <option values="France"> Italie </option>
+        <option values="France"> Royaume-Uni </option>
+        <option values="France"> Allemagne </option>
+        <option values="France"> Espagne </option>
+        <option values="France"> Mongolie </option>
+        <select>
+    <input type="checkbox" name="op1" values="values 2"/><br/>
+            <input type="submit" name="op1" values="values 2"/><br/>
+</form>
