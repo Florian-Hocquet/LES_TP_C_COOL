d3256db7a390f8fa91ca383df01071398547166a
h18000101 <florian.hocquet@etu.univ-amu.fr>
2019-09-25 09:45:20 +0200

    td1

--- TP2.php
+++ TP2.php
@@ -3,12 +3,13 @@ include 'utils.inc.php';
 ?>
 
 <form action="data-processing.php" method="post">
-    <input type="text" name="Id" value="Identifiant"/><br/>
-    <input type="radio" name="Civilité" value="Sexe"/><br/>
-    <input type="text" name="E-mail" value="E-mail"/><br/>
+    <input type="text" name="Id" value="Identifiant"/><br/><br/>
+    <input type="radio" name="Civilité" value="Homme"/><br/>
+    <input type="radio" name="Civilité" value="Homme"/><br/><br/>
+    <input type="text" name="E-mail" value="E-mail"/><br/><br/>
     <input type="password" name="mdp" value="Mot de passe"/><br/>
-    <input type="password" name="mdp" value="Mot de passe"/><br/>
-    <input type="text" name="tel" value="Téléphone"/><br/>
+    <input type="password" name="mdp" value="Mot de passe"/><br/><br/>
+    <input type="text" name="tel" value="Téléphone"/><br/><br/>
     <select name="Nom Pays">
         <option value="France"> France </option>
         <option value="France"> Italie </option>
@@ -16,7 +17,7 @@ include 'utils.inc.php';
         <option value="France"> Allemagne </option>
         <option value="France"> Espagne </option>
         <option value="France"> Mongolie </option>
-        </select>
-    <input type="checkbox" name="checkbox" value="check"/><br/>
+        </select><br/>
+    <input type="checkbox" name="checkbox" value="check"/><br/><br/>
     <input type="submit" name="action" value="mailer"/><br/>
 </form>
