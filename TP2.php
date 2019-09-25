910dd249d99712700760616d9410b2525c5485aa
h18000101 <florian.hocquet@etu.univ-amu.fr>
2019-09-25 09:46:03 +0200

    td1

--- TP2.php
+++ TP2.php
@@ -4,8 +4,8 @@ include 'utils.inc.php';
 
 <form action="data-processing.php" method="post">
     <input type="text" name="Id" value="Identifiant"/><br/><br/>
-    <input type="radio" name="Civilité" value="Homme"/><br/>
-    <input type="radio" name="Civilité" value="Homme"/><br/><br/>
+    <input type="radio" name="Civilité" value="Homme"/> Homme <br/>
+    <input type="radio" name="Civilité" value="Femme"/> Femme <br/><br/>
     <input type="text" name="E-mail" value="E-mail"/><br/><br/>
     <input type="password" name="mdp" value="Mot de passe"/><br/>
     <input type="password" name="mdp" value="Mot de passe"/><br/><br/>
