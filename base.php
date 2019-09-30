8361fb98fec06faba766cf75355fe842e36f4978
h18000101 <florian.hocquet@etu.univ-amu.fr>
2019-09-30 09:08:25 +0200

    td1

---
+++ base.php
@@ -0,0 +1,19 @@
+<?php
+$dbLink = mysqli_connect(dbHost, dbLogin, dbPass)
+or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
+
+mysqli_select_db($dbLink , dbBd)
+or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));
+
+$query = 'SELECT id, email, date FROM user';
+
+
+if (!($dbResult = mysqli_query($dbLink, $query))) {
+    echo 'Erreur de requête<br/>';
+// Affiche le type d'erreur.
+    echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+// Affiche la requête envoyée.
+    echo 'Requête : ' . $query . '<br/>';
+    exit();
+}
+?>
